23e2cfbf1405c69d094cbaee3bfddb2e93d45713
Daniel Bodenstein <danielboden111@gmail.com>
2023-06-27 12:27:14 +0300

    flask_finished

--- flaskApi/app.py
+++ flaskApi/app.py
@@ -1,42 +1,77 @@
+import logging
 from zipfile import BadZipFile
-import os
-from parser import get_presentation_as_list_of_slides
 from flask import Flask, request, jsonify
-import json
+import app_service
 
 app = Flask(__name__)
 
-def save_list_as_json(data_list, directory, filename):
-    if not os.path.exists(directory):
-        os.makedirs(directory)
+# Configure the logger
+logging.basicConfig(level=logging.DEBUG)
 
-    file_path = os.path.join(directory, filename)
-    with open(file_path, 'w') as file:
-        json.dump(data_list, file)
-
-    return file_path
 
 @app.route('/upload', methods=['POST'])
 def upload_file():
+    # Log request details
+    log_message = f"Request Details: Method={request.method}, URL={request.url}, IP={request.remote_addr}, User-Agent={request.user_agent}"
+    logging.info(log_message)
+
+    # check if file send
     if 'file' not in request.files:
-        return 'No file found', 400
+        error_msg = 'No file found in request!'
+        logging.error(error_msg)
+        return jsonify({'error_msg': error_msg}), 400
 
     file = request.files['file']
 
+    # Check if a file is selected
     if file.filename == '':
-        return 'No file selected', 400
+        error_msg = 'No file selected!'
+        logging.error(error_msg)
+        return jsonify({'error_msg': error_msg}), 400
 
     try:
-        slides = get_presentation_as_list_of_slides(file)
+        uid = app_service.upload_pptx(file)
     except (BadZipFile, ValueError) as e:
-        return "It's not a .pptx file", 400
+        error_message = str(e)
+        logging.error('Invalid file format: %s', error_message)
+        return jsonify({'error_msg': 'Invalid file format: ' + error_message}), 400
 
-    uid = '123'
-    r = save_list_as_json({"name": file.filename, "slides": slides}, './../files/slides_to_explain', uid)
+    response = {
+        'msg': 'File saved successfully!',
+        'UID': uid
+    }
 
-    #return jsonify({'b': slides})
-    return r
+    logging.info('File uploaded successfully with UID: %s', uid)
+    return jsonify(response), 200
 
 
-if __name__ == "__main__":
+@app.route('/explanation', methods=['GET'])
+def get_explanation():
+    uid = request.args.get('uid')
+
+    if not uid:
+        error_msg = 'Missing UID parameter'
+        logging.error(error_msg)
+        return jsonify({'error_msg': error_msg}), 400
+
+    if not app_service.check_if_uid_exist(uid):
+        error_msg = 'uid doesnt exist'
+        logging.error(error_msg)
+        return jsonify({'error_msg': error_msg}), 400
+
+    explanation = app_service.get_explanation_by_uid(uid)
+
+    if not explanation:
+        error_msg = f'explanation {uid} dont ready'
+        logging.error(error_msg)
+        return jsonify({'error_msg': error_msg}), 400
+
+    return jsonify({'explanation': explanation}), 200
+
+
+def run_api():
     app.run(debug=True)
+
+
+if __name__ == '__main__':
+    run_api()
